a2438356bc16a1fac630b5df0ee26e2ba598a77c
Hill <hill@kimo.com>
2016-05-15 13:00:33 +0000

    update template

--- generators/app/templates/test.php
+++ generators/app/templates/test.php
@@ -1,13 +1,17 @@
 <?php
-PMVC\Load::plug();
-PMVC\addPlugInFolders(['../']);
+namespace PMVC\PlugIn\<%= plugInName %>;
+use PHPUnit_Framework_TestCase;
+
+\PMVC\Load::plug();
+\PMVC\addPlugInFolders(['../']);
+
 class <%= PlugInName %>Test extends PHPUnit_Framework_TestCase
 {
     private $_plug = '<%= plugInName %>';
     function testPlugin()
     {
         ob_start();
-        print_r(PMVC\plug($this->_plug));
+        print_r(\PMVC\plug($this->_plug));
         $output = ob_get_contents();
         ob_end_clean();
         $this->assertContains($this->_plug,$output);
