d48e15976c0c3150cac7f34b543d22c9278c8a73
Hill <hill@kimo.com>
2019-02-17 12:04:06 +0000

    fixed phpunit template

--- generators/app/templates/tests/include.php
+++ generators/app/templates/tests/include.php
@@ -2,5 +2,17 @@
 
 $path = __DIR__.'/../vendor/autoload.php';
 include $path;
+
+if (!class_exists('PHPUnit_Framework_TestCase')) {
+    class PHPUnit_Framework_TestCase extends
+        \PHPUnit\Framework\TestCase
+    {
+    }
+    class PHPUnit_Framework_Error extends
+        \PHPUnit\Framework\Error\Notice
+    {
+    }
+}
+
 \PMVC\Load::plug();
 \PMVC\addPlugInFolders([__DIR__.'/../../']);
